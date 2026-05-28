6ebcf7dc3a3b3af893d2b8fd40566d0f084f7754
Komediruzecki <komediruzecki.2015@gmail.com>
2026-05-28 16:46:18 +0000

    fix: move support links into HelpModal header icon row
    Replace repo GitHub link with sponsors link.
    Add Ko-fi heart icon to the left of GitHub sponsors icon.
    Remove separate Support section below tours.

--- packages/app/src/components/HelpModal/HelpModal.tsx
+++ packages/app/src/components/HelpModal/HelpModal.tsx
@@ -173,9 +173,17 @@ function HelpModal(props: HelpModalProps) {
           <div class={ui.iconRow}>
             <a
               class={ui.iconLink}
-              href="https://github.com/chaos-matters/chaos-master"
+              href="https://ko-fi.com/chaosmatters"
               target="_blank"
-              title="GitHub"
+              title="Support on Ko-fi"
+            >
+              <Heart />
+            </a>
+            <a
+              class={ui.iconLink}
+              href="https://github.com/sponsors/chaos-matters"
+              target="_blank"
+              title="Sponsor on GitHub"
             >
               <GitHub />
             </a>
@@ -382,26 +390,6 @@ function HelpModal(props: HelpModalProps) {
         >
           Timeline Tour
         </button>
-      </div>
-
-      <h2 class={ui.sectionTitle}>Support</h2>
-      <div class={ui.supportLinks}>
-        <a
-          class={ui.iconLink}
-          href="https://ko-fi.com/chaosmatters"
-          target="_blank"
-          title="Support on Ko-fi"
-        >
-          <Heart />
-        </a>
-        <a
-          class={ui.iconLink}
-          href="https://github.com/sponsors/chaos-matters"
-          target="_blank"
-          title="Sponsor on GitHub"
-        >
-          <GitHub />
-        </a>
       </div>
 
       <div class={ui.gpuHeader}>
